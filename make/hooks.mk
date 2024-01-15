5dd9f351d02cc62875316e0c5533265db945ce88
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-15 11:32:04 -0600

    simplified the override variables for the hooks

--- make/hooks.mk
+++ make/hooks.mk
@@ -9,13 +9,13 @@ shared-hook-%:
 
 # Targets
 .PHONY: shared-hook-install
- hook-install: $(FNAL_ASIC_COMPUTE_REPO)
-	$(MAKE) -C $(FNAL_ASIC_COMPUTE_REPO) install WORKDIR_ROOT=$(WORKDIR_ROOT)
+shared-hook-install: $(FNAL_ASIC_COMPUTE_REPO)
+	$(MAKE) -C $(FNAL_ASIC_COMPUTE_REPO) install
 
 .PHONY: shared-hook-test
-hook-test: $(FNAL_ASIC_COMPUTE_REPO)
-	$(MAKE) -C $(FNAL_ASIC_COMPUTE_REPO) test WORKDIR_ROOT=$(WORKDIR_ROOT)
+shared-hook-test: $(FNAL_ASIC_COMPUTE_REPO)
+	$(MAKE) -C $(FNAL_ASIC_COMPUTE_REPO) test
 
 .PHONY: shared-hook-uninstall
-hook-uninstall: $(FNAL_ASIC_COMPUTE_REPO)
-	$(MAKE) -C $(FNAL_ASIC_COMPUTE_REPO) uninstall WORKDIR_ROOT=$(WORKDIR_ROOT)
+shared-hook-uninstall: $(FNAL_ASIC_COMPUTE_REPO)
+	$(MAKE) -C $(FNAL_ASIC_COMPUTE_REPO) uninstall
