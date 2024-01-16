16a1c6962e99805a9256946db3c8826a11515e1f
Jeff Fredenburg <jfreden@fnal.gov>
2024-01-15 19:32:42 -0600

    Updated source file locations to accomodate make-configurator-rules updates and added BINDIR to avoid implicit ~/.local/bin

--- make/private.mk
+++ make/private.mk
@@ -12,6 +12,7 @@ override VERSION := $(shell git describe --always --dirty --broken 2> /dev/null)
 DESTDIR =
 HOMEDIR = $(HOME)
 PREFIX = $(HOME)/.local
+BINDIR = $(PREFIX)/bin
 LIBDIR = $(PREFIX)/lib
 WORKDIR_ROOT := $(CURDIR)/.make
 
