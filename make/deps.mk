c3f4226541cb9307cc294d4ca0c81a1c7948e90d
Jeff Fredenburg <jfreden@fnal.gov>
2024-07-01 08:31:32 -0500

    updated the dependencies to include th new bowerbird dependencies

--- make/deps.mk
+++ make/deps.mk
@@ -1,23 +1,27 @@
 # Constants
-BOXERBIRD_BRANCH := main
-CONFIGURATOR_RULES_BRANCH := main
-FNAL_ASIC_COMPUTE_BRANCH := main
+WORKDIR_DEPS ?= $(error ERROR: Undefined variable WORKDIR_DEPS)
 
-WORKDIR_ROOT ?= $(error ERROR: Undefined variable WORKDIR_ROOT)
-WORKDIR_DEPS = $(WORKDIR_ROOT)/deps
+# Load Bowerbird Dependency Tools
+BOWERBIRD_DEPS.MK := $(WORKDIR_DEPS)/bowerbird-deps/bowerbird_deps.mk
+$(BOWERBIRD_DEPS.MK):
+	@curl --silent --show-error --fail --create-dirs -o $@ -L \
+https://raw.githubusercontent.com/ic-designer/make-bowerbird-deps/\
+main/src/bowerbird-deps/bowerbird-deps.mk
+include $(BOWERBIRD_DEPS.MK)
+
+# Load Dependencies
+$(call bowerbird::git-dependency,$(WORKDIR_DEPS)/bowerbird-help,\
+		https://github.com/ic-designer/make-bowerbird-help.git,main,bowerbird.mk)
+$(call bowerbird::git-dependency,$(WORKDIR_DEPS)/bowerbird-githooks,\
+		https://github.com/ic-designer/make-bowerbird-githooks.git,main,bowerbird.mk)
+$(call bowerbird::git-dependency,$(WORKDIR_DEPS)/bowerbird-install-tools,\
+		https://github.com/ic-designer/make-bowerbird-install-tools.git,main,bowerbird.mk)
+$(call bowerbird::git-dependency,$(WORKDIR_DEPS)/bowerbird-test,\
+		https://github.com/ic-designer/make-bowerbird-test.git,main,bowerbird.mk)
 
 # Dependencies
-override BOXERBIRD.MK := $(WORKDIR_DEPS)/make-boxerbird/boxerbird.mk
-$(BOXERBIRD.MK):
-	@echo "INFO: Fetching $@..."
-	git clone --config advice.detachedHead=false --depth 1 \
-			https://github.com/ic-designer/make-boxerbird.git --branch $(BOXERBIRD_BRANCH) \
-			$(WORKDIR_DEPS)/make-boxerbird
-	test -f $@
-	@echo "INFO: Fetching $@ completed."
-	@echo
-
-override CONFIGURATOR_RULES.MK := $(WORKDIR_DEPS)/make-configurator-rules/make-configurator-rules.mk
+CONFIGURATOR_RULES_BRANCH := main
+CONFIGURATOR_RULES.MK := $(WORKDIR_DEPS)/make-configurator-rules/make-configurator-rules.mk
 $(CONFIGURATOR_RULES.MK):
 	@echo "Loading FNAL ASIC Compute Rules..."
 	git clone --config advice.detachedHead=false --depth 1 \
@@ -26,7 +30,8 @@ $(CONFIGURATOR_RULES.MK):
 	test -f $@
 	@echo
 
-override FNAL_ASIC_COMPUTE_REPO := $(WORKDIR_DEPS)/fnal-asic-compute-$(FNAL_ASIC_COMPUTE_BRANCH)
+FNAL_ASIC_COMPUTE_BRANCH := main
+FNAL_ASIC_COMPUTE_REPO := $(WORKDIR_DEPS)/fnal-asic-compute-$(FNAL_ASIC_COMPUTE_BRANCH)
 $(FNAL_ASIC_COMPUTE_REPO):
 	@echo "Loading FNAL ASIC compute..."
 	git clone --config advice.detachedHead=false --depth 1 \
